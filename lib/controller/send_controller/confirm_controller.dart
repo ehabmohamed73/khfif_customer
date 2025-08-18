f1db1814ba54838c5a072f5f226347175be38906
Ehab Mohammed <65684752+ehabmohamed73@users.noreply.github.com>
2025-08-19 01:34:18 +0300

    updata

--- lib/controller/send_controller/confirm_controller.dart
+++ lib/controller/send_controller/confirm_controller.dart
@@ -4,8 +4,16 @@ import 'package:khafif/core/services/inti_services.dart';
 class ConfirmController extends GetxController {
   MyServices services = Get.find<MyServices>();
   String city = '';
+  String fromCity="";
+  var arguments=Get.arguments;
+  String? shipmentType='';
+  String? weightUnit='';
+  String? weightValue='';
   @override
   void onInit() {
+    shipmentType=arguments["shipmentType"];
+    weightUnit=arguments["weightUnit"];
+    weightValue=arguments["weightValue"];
     city = services.sharedpref.getString("city") ?? '';
     super.onInit();
   }
--- lib/view/screens/sened_package/confirm_and_checkout.dart
+++ lib/view/screens/sened_package/confirm_and_checkout.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:khafif/controller/send_controller/confirm_controller.dart';
+import 'package:khafif/core/constants/image_assets.dart';
+import 'package:khafif/view/widgets/calculator/custom_build_widget.dart';
 import 'package:khafif/view/widgets/calculator/custom_header_calc.dart';
 
 // ignore: must_be_immutable
 class ConfirmAndCheckout extends StatelessWidget {
   ConfirmAndCheckout({super.key});
-  ConfirmController confirmController = Get.put(ConfirmController());
+  ConfirmController controller = Get.put(ConfirmController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomHeaderCalc(title: "معلومات الشحنة"),
-      body: Container(
-        child: Text(
-          "المدينة: ${confirmController.city}",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: ListView(
+          children: [
+            Container(
+                  padding: const EdgeInsets.all(16),
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 16,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomBuildWidget(
+                        imageUrl: imagesAssets.fromCityImage,
+                        title: "المرسلة من",
+                        value: controller.fromCity,
+                      ),
+                      CustomBuildWidget(
+                        title: "المرسلة إلى",
+                        value: controller.city,
+                        imageUrl: imagesAssets.toCityImage,
+                      ),
+                      CustomBuildWidget(
+                        title: "ماذا تريد أن تشحن؟",
+                        value:
+                            "${controller.weightValue} كجم، ${controller.shipmentType}",
+                        imageUrl: imagesAssets.shipmentImage,
+                      ),
+                    ],
+                  ),
+                ),
+          ],
+        )
       ),
     );
   }
